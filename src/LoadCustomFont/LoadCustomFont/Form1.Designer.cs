ac43ebe6f25410f98ee8ea05bbca91dc75f176eb
pengyancai <pfkue126@126.com>
2017-11-11 14:08:44 +0800

    增加winform加载自定义字体文件

---
+++ src/LoadCustomFont/LoadCustomFont/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(95, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "虚拟现实";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(95, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "虚拟现实";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(95, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "虚拟现实";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(95, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "虚拟现实";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 273);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ src/LoadCustomFont/LoadCustomFont/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+/*        Load custom font by filename*/
+        public void setFontByFileName()
+        {
+            string AppPath = Application.StartupPath;
+            string fontPath = AppPath + @"\汉仪刚艺体-35W.ttf";
+            if (System.IO.File.Exists(fontPath))
+            {
+                System.Drawing.Text.PrivateFontCollection pfc = new System.Drawing.Text.PrivateFontCollection();
+                pfc.AddFontFile(fontPath);
+                Font myFont = new Font(pfc.Families[0], 20f);
+
+                this.label1.Font = myFont;
+            }
+        }
+
+/*        Load custom font by font file stream*/
+        public void setFontByStream()
+        {
+            string AppPath = Application.StartupPath;
+            string fontPath = AppPath + @"\汉仪刚艺体-35W.ttf";
+            if (System.IO.File.Exists(fontPath))
+            {
+                System.IO.Stream fileStream = new System.IO.StreamReader(fontPath).BaseStream;
+
+                byte[] bytes = new byte[fileStream.Length];
+                fileStream.Read(bytes, 0, bytes.Length);
+                // 设置当前流的位置为流的开始
+                fileStream.Seek(0, System.IO.SeekOrigin.Begin);
+                System.Drawing.Text.PrivateFontCollection pfc = new System.Drawing.Text.PrivateFontCollection();
+                unsafe
+                {
+                    fixed (byte* pFontData = &bytes[0])
+                    {
+                        pfc.AddMemoryFont((System.IntPtr)pFontData, bytes.Length);
+                    }
+                }
+
+                Font myFont = new Font(pfc.Families[0], 20f);
+                this.label3.Font = myFont;
+            }
+        }
+
+/*        load custom font from application resource*/
+        public void setFontFromResource()
+        {
+            byte[] bytes = LoadCustomFont.Properties.Resources.汉仪刚艺体_35W;
+
+             System.Drawing.Text.PrivateFontCollection pfc = new System.Drawing.Text.PrivateFontCollection();
+            IntPtr MeAdd = System.Runtime.InteropServices.Marshal.AllocHGlobal(bytes.Length);
+            System.Runtime.InteropServices.Marshal.Copy(bytes, 0, MeAdd, bytes.Length);
+            pfc.AddMemoryFont(MeAdd, bytes.Length);
+
+//             unsafe //打开"允许不安全代码编译"开关，此句才不报错  
+//             {
+//                 fixed (byte* pFontData = bytes)
+//                 {
+//                     pfc.AddMemoryFont((System.IntPtr)pFontData, bytes.Length);
+//                 }
+//             }
+            Font myFont = new Font(pfc.Families[0], 20f);
+            this.label4.Font = myFont;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            setFontByFileName();
+
+            setFontByStream();
+
+            setFontFromResource();
+        }
+    }
+}
